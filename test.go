a281329d657de7b6d64a0ba09b4cd0ce6972ceed
oliver <oliverdeng2019@gmail.com>
2021-08-24 20:26:54 +0800

    add test.go

--- test.go
+++ test.go
@@ -1,1 +1,9 @@
-func max(num1, num2 int) int {	var result int	if num1 > num2 {		result = num1	} else {		result = num2	}	return result}
+func max(num1, num2 int) int {
+	var result int
+	if num1 > num2 {
+		result = num1
+	} else {
+		result = num2
+	}
+	return result
+}
